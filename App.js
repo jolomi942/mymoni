7f870d99a94948f4c524ec2e5cc6af37ffc14c66
Mac <mac@Macs-MacBook-Air.local>
2022-11-17 12:13:08 +0100

    created some data

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  );clear
+  
 }
 
 const styles = StyleSheet.create({
